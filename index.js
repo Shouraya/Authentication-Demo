0b27b4f5b54517eb8e94ca07d3ac333b5cad2163
Shouraya <shourayagoyal2406@gmail.com>
2020-10-22 13:10:36 +0530

    added Login functionality

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -63,6 +64,20 @@ app.post("/register", function(req, res){
             });
         }
     });
+});
+
+//+++++++++++
+//LOGIN
+//+++++++++++
+app.get("/login", function(req, res){
+    res.render('login');
+});
+
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+    //leave empty for now
 });
 
 var PORT = process.env.PORT || 3000;
