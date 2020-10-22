9f7977d93c0982537372a14dcb230cd3f0c4601a
Shouraya <shourayagoyal2406@gmail.com>
2020-10-22 12:39:06 +0530

    local passport setup

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+require('dotenv').config();
 const express = require("express"),
-mongoose = require("mongoose");
+      mongoose = require("mongoose"),
+      passport = require("passport"),
+      bodyParser = require("body-parser"),
+      User = require("./models/user"),
+      LocalStrategy = require("passport-local"),
+      passportLocalMongoose = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost/auth_demo", {
 	useNewUrlParser: true,   //these are written to remove deprecations warning
     useUnifiedTopology: true,
     useFindAndModify: false,
 	useCreateIndex: true    
 });
+
 const app = express();
 app.set("view engine", 'ejs');
+app.use(require("express-session")({
+    secret : process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Setting Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req, res){
     res.render('home');
